355610cf9074ea0d46ea92eca1cda6a6ae457a1a
daniphant <daniphant@protonmail.com>
2021-12-11 15:56:46 -0300

    feat: add empty repository to disable error

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -2,7 +2,9 @@ export default (className: string, variablaName: string, useCaseFileName: string
   `import ${className}UseCase from './${useCaseFileName}';
 import ${className}Controller from './${controllerFileName}';
 
-const ${variablaName}UseCase = new ${className}UseCase();
+const ${variablaName}UseCase = new ${className}UseCase(
+  {} // Inject the repository here
+);
 
 const ${variablaName}Controller = new ${className}Controller(${variablaName}UseCase);
 
