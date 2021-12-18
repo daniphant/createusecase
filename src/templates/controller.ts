9b7f66f0f5f3f43e720b31c348c3b75dacd28f7f
daniphant <daniphant@protonmail.com>
2021-12-18 12:11:37 -0300

    feat: dto style customization

--- src/templates/controller.ts
+++ src/templates/controller.ts
@@ -1,17 +1,27 @@
-export default (className: string, variablaName: string, dtoFileName: string, useCaseFileName: string) => (
-  `import { Request, Response } from 'express';
-import ${className}UseCase from './${useCaseFileName}';
-import { ${className}RequestDto } from './${dtoFileName}';
+import * as vscode from "vscode";
 
-export default class ${className}Controller {
-  constructor(private readonly ${variablaName}UseCase: ${className}UseCase) {}
 
-  async handle(req: Request, res: Response): Promise<Response> {
-    const ${variablaName}RequestDto: ${className}RequestDto = new ${className}RequestDto(req.body);
 
-    const response = await this.${variablaName}UseCase.execute(${variablaName}RequestDto);
+export default (className: string, variablaName: string, dtoFileName: string, useCaseFileName: string) => {
+  const config = vscode.workspace.getConfiguration("createusecase");
+  const dtoStyle = config.get("dtoStyle") as string;
 
-    return res.status(200).json(response);
-  }
-}
-`);
+  return   `import { Request, Response } from 'express';
+  import ${className}UseCase from './${useCaseFileName}';
+  ${dtoStyle === "class with constructor" ? `import { ${className}RequestDto } from './${dtoFileName}';` : ""}
+  
+  export default class ${className}Controller {
+    constructor(private readonly ${variablaName}UseCase: ${className}UseCase) {}
+  
+    async handle(req: Request, res: Response): Promise<Response> {
+      ${
+        dtoStyle === "class with constructor" ? 
+          `const ${variablaName}RequestDto = new ${className}RequestDto(req.body.name);
+      const response = await this.${variablaName}UseCase.execute(${variablaName}RequestDto);`
+        : `const response = await this.${variablaName}UseCase.execute(req.body);`
+      }
+  
+      return res.status(200).json(response);
+    }
+  }`;
+};
--- src/templates/dto.ts
+++ src/templates/dto.ts
@@ -1,8 +1,14 @@
-export default (className: string) => (
-`export class ${className}RequestDto {
+import * as vscode from 'vscode';
+
+export default (className: string) => {
+  const config = vscode.workspace.getConfiguration("createusecase");
+  const dtoStyle = config.get("dtoStyle") as string;
+
+  return `export ${dtoStyle === "interface" ? "interface ": "class "}${className}RequestDto {
   private name: string;
 
-  constructor({ name }) {
+  ${dtoStyle === "class with constructor" ? `constructor(name: string) {
     this.name = name;
-  }
-}`);
+  }` : ``}
+}`;
+};
